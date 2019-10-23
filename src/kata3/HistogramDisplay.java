0339ac150b18a3429d94425537d3a90c3f3d3009
Usuario <Usuario@10.22.148.98>
2019-10-23 13:43:15 +0100

    Kata3

---
+++ src/kata3/HistogramDisplay.java
@@ -0,0 +1,47 @@
+package kata3;
+
+import java.awt.Container;
+import java.awt.Dimension;
+import javax.swing.JPanel;
+import org.jfree.chart.ChartFactory;
+import org.jfree.chart.ChartPanel;
+import org.jfree.chart.JFreeChart;
+import org.jfree.chart.plot.PlotOrientation;
+import org.jfree.data.category.DefaultCategoryDataset;
+import org.jfree.ui.ApplicationFrame;
+
+
+public class HistogramDisplay extends ApplicationFrame{
+
+private Histrogram<String> histogram;
+    
+public HistogramDisplay(Histrogram<String> histogram) {
+super("HISTOGRAMA");
+this.histogram = histogram;
+this.setContentPane(createPanel());
+this.pack();
+}
+
+public void execute() {
+setVisible(true);
+}
+private JPanel createPanel() {
+ChartPanel chartpanel = new ChartPanel(createChart(createDataSet()));
+chartpanel.setPreferredSize(new  Dimension(500,400));
+return chartpanel;
+}
+
+private JFreeChart createChart(DefaultCategoryDataset dataset) {
+    JFreeChart chart = ChartFactory.createBarChart("Histograma JFreeChar","Dominios mails",
+            "Nº de mails", dataset, PlotOrientation.VERTICAL, false , false,rootPaneCheckingEnabled);
+    return chart;
+}
+private DefaultCategoryDataset createDataSet(){
+    DefaultCategoryDataset dataSet = new DefaultCategoryDataset();
+    for(String key : this.histogram.keySet()){
+        dataSet.addValue(this.histogram.get(key), "", key);
+    }
+    return dataSet;
+            
+}
+}
---
+++ src/kata3/Kata3.java
@@ -0,0 +1,20 @@
+
+package kata3;
+
+public class Kata3 {
+
+    
+    public static void main(String[] args) {
+        Histrogram histogram = new Histrogram();
+        histogram.increment("ulpgc.es");
+        histogram.increment("dis.ulpgc.es");
+        histogram.increment("ulpgc.es");
+        histogram.increment("google.es");
+        histogram.increment("ulpgc.es");
+        histogram.increment("dis.ulpgc.es");
+        histogram.increment("ulpgc.es");
+        HistogramDisplay hist = new HistogramDisplay(histogram);
+        hist.execute();
+    }
+    
+}
